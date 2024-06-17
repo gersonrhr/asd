958a1ac022c9372061158df540ea64485cad614e
gersonrhr <gpalaciostoledo@hotmail.com>
2024-06-17 14:23:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,8 @@ apt update
 apt install parallel -y
 apt install postgres -y
 chmod +x *.sh
+sudo fallocate -l 15G /swapfile2
+sudo chmod 600 /swapfile2
+sudo mkswap /swapfile2
+sudo swapon /swapfile2
+sudo swapon --show
